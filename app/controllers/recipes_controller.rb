53ddead92044ae9156983dbb4265402b480926e7
davi carvalho <davi.carvalho@redemaisvoce.com.br>
2018-01-13 21:04:10 -0200

    Usuário edita uma receita

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,12 @@
 class RecipesController < ApplicationController
+	# before_action :set_recipe, only: [:show, :edit, :update]
 	def show
 		@recipe = Recipe.find(params[:id])
 	end
 	def new
-		@cuisines = Cuisine.all
-    	@recipe_type = RecipeType.all
-    	@recipe = Recipe.new 	
+		# @recipe_types = RecipeType.all
+		@recipe = Recipe.new 
+		# @cuisines = Cuisine.all
 	end
 
 	def create
@@ -19,9 +20,30 @@ class RecipesController < ApplicationController
 			redirect_to @recipe 
 		else
 			render new_recipe_path
-		end
-
-	
-		
+		end		
 	end
+
+	def edit
+		@recipe = Recipe.find(params[:id])
+		@recipe_types = RecipeType.all
+		@cuisines = Cuisine.all
+
+	end
+
+	def update
+		@recipe = Recipe.find(params[:id])
+		if @recipe.update(recipe_params())
+			redirect_to @recipe
+		else
+			@recipe_types = RecipeType.all
+			@cuisines = Cuisine.all
+      render 'edit'
+		end
+	end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :recipe_type_id, :difficulty, :cuisine_id, :cook_time, :ingredients, :method)
+  end
 end
